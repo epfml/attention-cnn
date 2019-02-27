d86b4c547930384d6b7925d13bffc7ee0cad716b
Thijs Vogels <thijs.vogels@epfl.ch>
2019-02-27 08:49:55 +0100

    Add accuracy baseline for the ResNet18 baseline

--- hyperparameters/resnet18_cifar10_baseline.py
+++ hyperparameters/resnet18_cifar10_baseline.py
@@ -12,4 +12,20 @@ config = dict(
     seed=42,
 )
 
-test_accuracy = -1 # @todo
+best_test_accuracy = 0.9475999808311465
+best_test_accuracy_reached_at_epoch = 279
+
+final_test_accuracy = 0.9468999809026717 # at epoch 299, of course
+
+# if __name__ == '__main__':
+#     import sys
+#     import os
+#     sys.path.append('..')
+#     import train
+
+#     train.output_dir = os.path.join('output', os.path.basename(__file__))
+#     os.makedirs(train.output_dir)
+#     train.config = config
+#     best_accuracy = train.main()
+#     with open(os.path.join(train.output_dir, 'best_accuracy.txt'), 'w') as fp:
+#         fp.write(str(best_accuracy))
